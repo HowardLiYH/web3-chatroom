c8344da0ea2ab69dfbfaab310ee6b5e836dbc634
Yuhao Li <yuhaoli0204@gmail.com>
2023-11-19 01:20:01 +0800

    Second  web update

--- base/views.py
+++ base/views.py
@@ -1,22 +1,21 @@
 from django.shortcuts import render
+from base.models import Room  # Adjust the import statement based on the directory structure
 
 # Create your views here.
 
-rooms = [
-    {'id': 1, 'name': 'Lets talk about Bitcoin'},
-    {'id': 2, 'name': 'NFT Collector Playground'},
-    {'id': 3, 'name': 'Technical Analysis Education'},
-]
+# rooms = [
+#     {'id': 1, 'name': 'Lets talk about Bitcoin'},
+#     {'id': 2, 'name': 'NFT Collector Playground'},
+#     {'id': 3, 'name': 'Technical Analysis Education'},
+# ]
 
 
 def home(request):
+    rooms = Room.objects.all()  # pylint: disable=no-member
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
-    context = {'room':room}
+    room = Room.objects.get(id=pk)  # pylint: disable=no-member
+    context = {'room': room}
     return render(request, 'base/room.html', context)
